a47e64a12a5c986e36e5f00c145209d9cd411f27
mgbmix <mondomgbmix@gmail.com>
2023-10-18 00:44:20 -0400

    Quartz sync: Oct 18, 2023, 12:44 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "mgbmix.github.io/mgbmix",
+    baseUrl: "www.mgbmix.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
