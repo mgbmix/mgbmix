bb25f13453ec424a9dedccbb804f9d304e1ecff4
mgbmix <mondomgbmix@gmail.com>
2023-10-30 04:06:28 -0400

    Quartz sync: Oct 30, 2023, 4:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     baseUrl: "www.mgbmix.com",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**/private"],
     defaultDateType: "created",
     theme: {
       typography: {
